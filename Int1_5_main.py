41f4b9e8f241d7fc771690bb7c61272e56892c3b
Adele <chamouxadele@gmail.com>
2024-04-17 10:44:51 +0200

    file name change & imports

--- Int1_5_main.py
+++ Int1_5_main.py
@@ -1,11 +1,13 @@
 from lib import *
+import Int1_5_algorithms as algo
+import Int1_5_lib as libr
 
 if __name__ == "__main__":
-    welcome_print()
+    libr.welcome_print()
     actions = ['Display automata', 'Exit']
     selected_action = None
     while selected_action != 1: # to replace with the index of 'exit' action when actions are changed
-        selected_action = menu(actions)
+        selected_action = libr.menu(actions)
         match selected_action:
             case 0:
                 print('todo')
