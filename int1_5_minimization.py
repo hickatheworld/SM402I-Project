0c83b48e9544224dbb31fc906f63528ac1831d22
iri-rsl <iri.rasolo@gmail.com>
2024-04-19 19:38:54 +0200

    Corrections minimization & Display_minimal_automaton (need to be tested)

--- int1_5_minimization.py
+++ int1_5_minimization.py
@@ -18,17 +18,19 @@ def display_partition(partition, step):
         print()
 
 
-def detect_pattern(partition: list, index_sub_partition: int, a: dict):
+def detect_pattern(partition: list, index_sub_partition: int, automaton: dict):
     """
-        to detect the patterns of destination in ONE SUBSET of the partition
+        Detects the patterns of the destinations in ONE SUBSET of the partition
+        Args : The partition, the index of the considered sub-partition, the original automaton
+        Returns : A list of sublists which are the patterns of each sub-partitions e.g. [[1,0,2], [0,0,2]]
     """
     pattern_of_subset = []
-    letters = a['alphabet']
+    letters = automaton['alphabet']
     for i in range(len(partition[index_sub_partition])):
         pattern_of_state = []
         for j in range(len(letters)):
             # how to get the destination of this transition
-            dest = [t['to'] for t in a['transitions'] if t['from'] == partition[index_sub_partition][i]
+            dest = [t['to'] for t in automaton['transitions'] if t['from'] == partition[index_sub_partition][i]
                     and t['input'] == letters[j]]
             # now get the index of the sublist in which dest is
             dest_index = [k for k in range(len(partition)) if dest[0] in partition[k]]
@@ -68,12 +70,15 @@ def detect_pattern(partition: list, index: int, a: dict):
 '''
 
 
-def display_transitions(partition: list, a: dict, pattern_of_partition: list, step: int):
+def display_transitions(partition: list, automaton: dict, pattern_of_partition: list, step: int):
     """
-        should display the transitions of partitions
+        Displays the transitions of the states according to the sub-partitions in the given partition
+        Args : The considered partition, the original automaton, the list of patterns of the partition,
+            the step (number of times we calculate a new partition
+        Returns : None
     """
     print(f"Here are the transitions of each sub_partition according to θ[{step}]:")
-    letters = a["alphabet"]
+    letters = automaton["alphabet"]
     # First line with the alphabet
     print(end="|{:^10}".format(""))
     for letter in letters:
@@ -90,15 +95,17 @@ def display_transitions(partition: list, a: dict, pattern_of_partition: list, st
     print("\n")
 
 
-def repartition(partition: list, index: int, a: dict):
+def repartition(partition: list, index: int, automaton: dict):
     """
-        should return a list (either partition[i] itself, or a list w. sublists)
+        Given the sub-partition of a partition => it will divide it or not according to the patterns of the states
+        Args : The partition, the index of the sub-partition to be analysed, the original automaton
+        Returns : A list (either [partition[i]] itself, or a list w. sublists)
     """
     if len(partition[index]) == 1:
-        pattern_of_subset = detect_pattern(partition, index, a)
+        pattern_of_subset = detect_pattern(partition, index, automaton)
         return [partition[index]], pattern_of_subset
 
-    pattern_of_subset = detect_pattern(partition, index, a)
+    pattern_of_subset = detect_pattern(partition, index, automaton)
     final_sub_partition = [[partition[index][0]]]
 
     for i in range(1, len(partition[index])):
@@ -122,6 +129,11 @@ def repartition(partition: list, index: int, a: dict):
 
 
 def minimization(cdfa: dict):
+    """
+        Minimizes the given automaton (that should already be a CDFA)
+        Args : the CDFA
+        Returns : Already_Minimal (bool), mcdfa (dict), partition (list of sub-partitions)
+    """
     print("We have to minimize this automaton :")
     Algo.display_automaton(cdfa)
     # Check if the automaton is complete & cdfa
@@ -154,19 +166,21 @@ def minimization(cdfa: dict):
                 if partition[i] == sub_partition_i[0]:
                     temp_partition.append(sub_partition_i[0])
                     temp_partition_state.append(True)
-                    temp_pattern_of_partition.append(pattern_of_subset)
                 else:
                     for j in range(len(sub_partition_i)):
                         temp_partition.append(sub_partition_i[j])
                         temp_partition_state.append(False)
-                        temp_pattern_of_partition.append(pattern_of_subset)
             else:
+                pattern_of_subset = detect_pattern(partition, i, cdfa)
                 temp_partition.append(partition[i])
                 temp_partition_state.append(True)
+            temp_pattern_of_partition.append(pattern_of_subset)
 
         partition = temp_partition
         partition_state = temp_partition_state
         pattern_of_partition = temp_pattern_of_partition
+
+    print("Pattern of partition is : ", pattern_of_partition)
 
     # Verifying if the automaton was already minimal
     if len(partition) == len(cdfa['states']):
@@ -186,18 +200,23 @@ def minimization(cdfa: dict):
                 mcdfa["finalStates"].append(chr(65+i))
     # must define all the transitions (using pattern_of_partition ?)
     for i in range(len(pattern_of_partition)):
-        for j in range(len(pattern_of_partition(i))):
-            transition = {"from": chr(65+i), "input": mcdfa["alphabet"][j], "to": chr(65+pattern_of_partition[i][j])}
+        for j in range(len(pattern_of_partition[i])):
+            transition = {"from": chr(65+i), "input": mcdfa["alphabet"][j], "to": chr(65+pattern_of_partition[i][0][j])}
             mcdfa["transitions"].append(transition)
     return False, mcdfa, partition
 
 
-# def display_minimal_automaton(mcdfa):
+def display_minimal_automaton(already_minimal: bool, mcdfa: dict, partition):
+    if already_minimal:
+        print("No changes is needed !: ")
+        Algo.display_automaton(mcdfa)
+    else:
+        print("MINIMAL AUTOMATON: ")
+        display_partition(partition, "final")
+        Algo.display_automaton(mcdfa)
 
-
-
-# TEST
 '''
+# TEST
 automata = {'id': '16', 'states': ['1', '2', '3', '4', '5', '6'], 'alphabet': ['a', 'b', 'c', 'd'],
             'transitions': [{'from': '1', 'input': 'a', 'to': '2'}, {'from': '1', 'input': 'b', 'to': '6'},
                             {'from': '1', 'input': 'c', 'to': '6'}, {'from': '1', 'input': 'd', 'to': '6'},
@@ -212,5 +231,6 @@ automata = {'id': '16', 'states': ['1', '2', '3', '4', '5', '6'], 'alphabet': ['
                             {'from': '6', 'input': 'a', 'to': '6'}, {'from': '6', 'input': 'b', 'to': '6'},
                             {'from': '6', 'input': 'c', 'to': '6'}, {'from': '6', 'input': 'd', 'to': '6'}],
             'initialStates': ['1'], 'finalStates': ['5']}
-minimization(automata)
+a, m, p = minimization(automata)
+display_minimal_automaton(a, m,p)
 '''
